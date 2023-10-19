12d81a28813d186a736a8a07fdec17d9527ebfa7
Jitesh <jiteshnayak2004@gmail.com>
2023-10-19 21:34:31 +0530

    modified design

--- dadda_design.v
+++ dadda_design.v
@@ -7,26 +7,45 @@ module dadda_multiplier (
   wire [7:0] pp[3:0][3:0];
   wire [7:0] gi[3:0][3:0];
   wire [7:0] go[3:0][3:0];
+  wire [7:0] pa[3:0][3:0];
+  wire [7:0] go_prev[3:0][3:0];
   
-  // Generate partial products
-  // In this simple example, we directly connect the inputs to the partial products
-  // In a real Dadda multiplier, you'd generate the partial products differently
-  assign pp[0][0] = A[0] & B[0];
-  assign pp[0][1] = A[0] & B[1];
-  assign pp[0][2] = A[0] & B[2];
-  assign pp[0][3] = A[0] & B[3];
-
-  // Continue generating other partial products...
+  // Generate partial products using bitwise AND operations
+  genvar i, j;
+  generate
+    for (i = 0; i < 4; i = i + 1) begin
+      for (j = 0; j < 4; j = j + 1) begin
+        assign pp[i][j] = A[i] & B[j];
+      end
+    end
+  endgenerate
 
   // Generate Gi and Go signals
-  // In this example, we directly connect to 0
-  assign gi[0][0] = 1'b0;
-  assign go[0][0] = 1'b0;
+  generate
+    for (i = 0; i < 3; i = i + 1) begin
+      for (j = 0; j < 4; j = j + 1) begin
+        assign gi[i][j] = (pp[i][j] & pp[i+1][j]) | (go[i][j] & go_prev[i][j]);
+        assign go[i+1][j] = (pp[i][j] | pp[i+1][j]) & go[i][j];
+      end
+    end
+  endgenerate
 
-  // Continue generating Gi and Go signals...
+  // Accumulate the partial products
+  generate
+    for (i = 0; i < 8; i = i + 1) begin
+      for (j = 0; j < 4; j = j + 1) begin
+        if (i < 4) begin
+          assign pa[i][j] = pp[i][j];
+        end else begin
+          assign pa[i][j] = pp[i-4][j] << (i - 4);
+        end
+      end
+    end
+  endgenerate
 
   // Generate the final product
-  assign product = {pp[0][0], pp[0][1], pp[0][2], pp[0][3], pp[1][0], pp[1][1], pp[1][2], pp[1][3]};
+  always @* begin
+    product = pa[0][0] + pa[0][1] + pa[0][2] + pa[0][3] + pa[1][0] + pa[1][1] + pa[1][2] + pa[1][3];
+  end
 
 endmodule
-
